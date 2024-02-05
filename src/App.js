db90889d6882b6c5a39ca9aae43c32cee2d59d69
Mohammad111300 <mammadelec@gmail.com>
2024-02-05 13:25:33 +0330

    3

--- src/App.js
+++ src/App.js
@@ -1,55 +1,24 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  let [ toDoList, setToDoLists] = useState([]);
-  let [ newTask, setTask] = useState("");
-
-
-  const inputChange = (event) => {
-    setTask(event.target.value);
+  const [data, setData] = useState("");
+  
+  const exuse = (forwhat) => {
+    axios.get(`https://excuser-three.vercel.app/v1/excuse/${forwhat}/`).then((res) => {
+      setData(res.data[0].excuse);
+    });
   };
 
-  const addTask = () => {
-    const task = {
-      id: toDoList.length === 0 ? 1 : toDoList[toDoList.length - 1].id + 1,
-      taskName: newTask,
-      complited: false,
-    }
-    setToDoLists([...toDoList, task]);
-  }
 
-  const deleteTask = (id) => {
-    setToDoLists(toDoList.filter((task) => task.id !== id));
-  }
-
-  const taskComplited = (id) => {
-    setToDoLists(toDoList.map((task) => {
-      if (task.id === id){
-        return {...task, complited: true};
-      } else{
-        return task
-      } 
-    }))
-  }
 
   return (
     <div className="App">
-      <div>
-        <input type='text' onChange={inputChange}/>
-        <button onClick={addTask}>Submit</button>
-      </div>
-      <div>
-        {toDoList.map((task) => {
-          return (
-            <div style={{background: task.complited ? "green" : "white"}}>
-              <h2>{task.taskName}</h2>
-              <button onClick={() => {deleteTask(task.id)}}>X</button>
-              <button onClick={() => {taskComplited(task.id)}}>Complited</button>
-            </div>
-          )
-        })}
-      </div>
+      <button onClick={() => {exuse("party")}}>Party</button>
+      <button onClick={() => {exuse("family")}}>Family</button>
+      <button onClick={() => {exuse("office")}}>Office</button>
+      <p>{data}</p>
     </div>
   );
 }
